ed2f0a31560d9ee46a785e2633a91b9679b19fb4
Tony McCrae <tony@eelpieconsulting.co.uk>
2016-04-17 11:49:56 +0100

    Correct body parser.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,7 +4,7 @@ package controllers
 import mqtt.MQTTService
 import play.api.Logger
 import play.api.libs.json.Json
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{BodyParsers, Action, Controller}
 
 import scala.concurrent.Future
 
@@ -12,7 +12,7 @@ object Application extends Controller {
 
   val mqttService = MQTTService
 
-  def webhook = Action.async(parse.json) { request =>
+  def webhook = Action.async(BodyParsers.parse.json) { request =>
     val message = request.body.as[String]
     Logger.info("Received webhook: " + message)
     mqttService.publish(message)
