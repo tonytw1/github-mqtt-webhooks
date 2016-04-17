ee37d0f206fb1d4c01d1436395090a13d5b18b7d
Tony McCrae <tony@eelpieconsulting.co.uk>
2016-04-17 11:55:44 +0100

    Incorrect cast to string.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,7 +3,7 @@ package controllers
 
 import mqtt.MQTTService
 import play.api.Logger
-import play.api.libs.json.Json
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{BodyParsers, Action, Controller}
 
 import scala.concurrent.Future
@@ -13,9 +13,9 @@ object Application extends Controller {
   val mqttService = MQTTService
 
   def webhook = Action.async(BodyParsers.parse.json) { request =>
-    val message = request.body.as[String]
+    val message: JsValue = request.body
     Logger.info("Received webhook: " + message)
-    mqttService.publish(message)
+    mqttService.publish(message.toString())
     Future.successful(Ok(Json.toJson("Thanks!")))
   }
 
