ef73357c978dfd614bcc470d29cdf988935bc3a7
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-03-13 17:59:57 +0000

    Clean up. Rename the cafile config property to be more descriptive.

--- app/mqtt/MQTTService.scala
+++ app/mqtt/MQTTService.scala
@@ -1,26 +1,24 @@
 package mqtt
 
-import java.io.{FileInputStream, InputStream}
+import java.io.{File, FileInputStream}
 import java.security.KeyStore
 import java.security.cert.{CertificateFactory, X509Certificate}
 import javax.net.ssl.{SSLContext, TrustManagerFactory}
-import java.io.File
 
-import org.fusesource.mqtt.client.{BlockingConnection, MQTT, QoS}
+import org.fusesource.mqtt.client.{MQTT, QoS}
 import play.api.Play.current
 import play.api.{Logger, Play}
 
 object MQTTService {
 
-  private val host: String = Play.configuration.getString("mqtt.host").get
-  private val port: Int = Play.configuration.getInt("mqtt.port").get
-  private val cert: Option[String] = Play.configuration.getString("mqtt.tls.cert")
-
+  private val host = Play.configuration.getString("mqtt.host").get
+  private val port = Play.configuration.getInt("mqtt.port").get
   private val topic = Play.configuration.getString("mqtt.topic").get
+  private val caCert = Play.configuration.getString("mqtt.tls.cacert")
 
   def publish(message: String) = {
-    Logger.info("Publishing to mqtt topic " + topic + ": " + message)
-    val connection: BlockingConnection = getClient().blockingConnection
+    Logger.info("Publishing to mqtt topic " + host + ":" + port + " / " + topic + ": " + message)
+    val connection = getClient().blockingConnection
     connection.connect
     connection.publish(topic, message.getBytes, QoS.AT_MOST_ONCE, false)
     connection.disconnect
@@ -29,28 +27,29 @@ object MQTTService {
   private def getClient(): MQTT = {
 
     def sslContext(certPath: String): SSLContext = {
-      val cf: CertificateFactory = CertificateFactory.getInstance("X.509")
-      val asStream: InputStream = new FileInputStream(new File(certPath))
-      val caCert: X509Certificate = cf.generateCertificate(asStream).asInstanceOf[X509Certificate]
-      val tmf: TrustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
-      val ks: KeyStore = KeyStore.getInstance(KeyStore.getDefaultType)
+      val cf = CertificateFactory.getInstance("X.509")
+      val asStream = new FileInputStream(new File(certPath))
+      val caCert = cf.generateCertificate(asStream).asInstanceOf[X509Certificate]
+      val tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
+      val ks = KeyStore.getInstance(KeyStore.getDefaultType)
       ks.load(null)
       ks.setCertificateEntry("caCert", caCert)
       tmf.init(ks)
-      val sslContext: SSLContext = SSLContext.getInstance("TLS")
+      val sslContext = SSLContext.getInstance("TLS")
       sslContext.init(null, tmf.getTrustManagers, null)
       sslContext
     }
 
-    cert.fold {
-      val mqtt: MQTT = new MQTT()
+    caCert.fold {
+      val mqtt = new MQTT()
       mqtt.setHost(host, port)
       mqtt
     } { c =>
-      val mqtt: MQTT = new MQTT()
+      val mqtt = new MQTT()
       mqtt.setHost("tls://" + host + ":" + port)
       mqtt.setSslContext(sslContext(c))
       mqtt
     }
   }
+
 }
